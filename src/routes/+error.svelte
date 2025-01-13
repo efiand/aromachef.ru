ccb209b5c6b9657765f9f5e52ccd806a18b14f4e
Andrey Efimov <efiand@ya.ru>
2025-01-13 15:39:51 +0300

    Правки от SEO

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <div class="content">
-	<h1>{page.status}</h1>
+	<h1>Ошибка {page.status}</h1>
 
 	{#if page.error}
 		<p>{page.error.message}</p>
 	{/if}
+
+	<p>
+		<a href="mailto:efiand@ya.ru?subject=aromachef">
+			Связаться с разработчиком
+		</a>
+	</p>
 </div>
