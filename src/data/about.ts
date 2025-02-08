44805594e9924071b95726b996bee0bf86ab96a0
Andrey Efimov <efiand@ya.ru>
2025-02-08 20:11:56 +0300

    Дополнения от заказчика

--- src/data/about.ts
+++ src/data/about.ts
@@ -9,7 +9,7 @@ const aboutData = {
 			Здравствуйте! Я очень рада, что вы заглянули в гости на мою Аромакухню.
 		</p>
 		<p>
-			Меня зовут Ольга. Я сертифицированный специалист по <a href="${PICTURE_URL}/common/aromachef-cert.webp" target="_blank">АромаТестированию</a> и <a href="https://fs-thb03.getcourse.ru/fileservice/file/thumbnail/h/b92216eef2409b7e6286a70f15ddd5e1.png/s/800x/a/559935/sc/196" target="_blank" rel="nofollow noopener">эфирным маслам doTERRA</a>. А ещё я очень люблю готовить.
+			Меня зовут Ольга. Я сертифицированный специалист по <a href="${PICTURE_URL}/common/aromachef-cert.webp" target="_blank">АромаТестированию</a> и <a href="${PICTURE_URL}/common/aromachef-cert-doterra.webp" target="_blank">эфирным маслам doTERRA</a>. Имею <a href="${PICTURE_URL}/common/aromachef-diplom.webp" target="_blank">диплом</a> о профессиональной переподготовке по программе «Специалист по ароматестированию». А ещё я очень люблю готовить.
 		</p>
 		<p>
 			Хозяйки знают, что самое сложное в приготовлении еды — это каждый день готовить вкусно и разнообразно, использовать при этом самые простые и доступные продукты и не тратить на приготовление еды много времени. Раньше в этом помогали специи и травы, а теперь на помощь пришли и эфирные масла.
