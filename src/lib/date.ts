a8c0dca78682ac099b7576b47fa9df88f5d1feeb
Andrey Efimov <efiand@ya.ru>
2025-02-09 20:12:24 +0300

    Даты в sitemap - пока по датам публикаций

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -5,4 +5,9 @@ function toRFC822(dateString: dayjs.ConfigType) {
 	return dayjs(dateString).format('ddd, DD MMM YYYY hh:mm:ss +0300');
 }
 
-export { toRFC822 };
+/** Время в формате W3C Datetime (1997-07-16T19:20:30+03:00) */
+function toW3CDatetime(dateString: dayjs.ConfigType) {
+	return dayjs(dateString).format('YYYY-MM-DDThh:mm:ss+03:00');
+}
+
+export { toRFC822, toW3CDatetime };
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,49 +1,132 @@
+import { toW3CDatetime } from '@/lib/date';
 import { minifyInternal } from '@/lib/minify';
 import { prisma } from '@/lib/prisma';
 
 import type { RequestHandler } from './$types';
 
+type SitemapCache = {
+	[key: number]: SitemapItem & { lastRecipeId: number };
+};
+
+type SitemapItem = {
+	changefreq?:
+		| 'always'
+		| 'daily'
+		| 'hourly'
+		| 'monthly'
+		| 'never'
+		| 'weekly'
+		| 'yearly';
+	lastmod?: string;
+	page: string;
+	priority?:
+		| '0.0'
+		| '0.1'
+		| '0.2'
+		| '0.3'
+		| '0.4'
+		| '0.5'
+		| '0.6'
+		| '0.7'
+		| '0.8'
+		| '0.9'
+		| '1.0';
+};
+
+const BEGIN_TEMPLATE = `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">`;
+
 async function GET({ url: { hostname } }: Parameters<RequestHandler>[0]) {
-	function mapPages(page: string) {
-		return `<url><loc>https://${hostname}/${page}</loc></url>`;
-	}
+	const structuresCache: SitemapCache = {};
+	const tagsCache: SitemapCache = {};
 
-	const [recipes, tags] = await Promise.all([
-		prisma.recipes.findMany({
-			select: { id: true, structureId: true },
-			where: { published: true }
-		}),
-		prisma.tags.findMany({
-			select: { id: true },
-			where: { recipes: { some: { recipe: { published: true } } } }
-		})
-	]);
-
-	const pages: string[] = [
-		'',
-		'about',
-		'important',
-		'structure',
-		...tags.map(function ({ id }) {
-			return `tag/${id}`;
-		})
-	];
-	const uniqueStructures = new Set<string>([]);
-
-	recipes.forEach(function ({ id, structureId }) {
-		pages.push(`recipe/${id}`);
-		uniqueStructures.add(`structure/${structureId}`);
+	const recipes = await prisma.recipes.findMany({
+		orderBy: { publishedAt: 'desc' },
+		select: {
+			id: true,
+			publishedAt: true,
+			structureId: true,
+			tags: { select: { tagId: true } }
+		},
+		where: { published: true }
 	});
-	pages.push(...uniqueStructures);
+	const lastRecipeMod = toW3CDatetime(recipes[0].publishedAt);
 
-	const body = minifyInternal(`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">
-			${pages.map(mapPages).join('')}
-		</urlset>
-	`);
+	const pages: SitemapItem[] = [
+		{
+			changefreq: 'weekly',
+			lastmod: lastRecipeMod,
+			page: hostname,
+			priority: '0.8'
+		},
+		{ page: `${hostname}/structure`, priority: '0.8' },
+		{ page: `${hostname}/important` },
+		{ page: `${hostname}/about` }
+	];
 
-	return new Response(body, { headers: { 'Content-Type': 'application/xml' } });
+	let body = BEGIN_TEMPLATE;
+
+	pages.forEach(function (page) {
+		body += getItemTemplate(page);
+	});
+	recipes.forEach(function ({ id: recipeId, publishedAt, structureId, tags }) {
+		const lastmod = toW3CDatetime(publishedAt);
+		const cache = { lastmod, lastRecipeId: recipeId };
+
+		// Обновление данных о разделе
+		if (!structuresCache[structureId]) {
+			structuresCache[structureId] = {
+				...cache,
+				page: `${hostname}/structure/${structureId}`
+			};
+		}
+
+		// Обновление данных о тегах
+		tags.forEach(function ({ tagId }) {
+			if (!tagsCache[tagId]) {
+				tagsCache[tagId] = {
+					...cache,
+					page: `${hostname}/tag/${tagId}`
+				};
+			}
+		});
+
+		body += getItemTemplate({
+			changefreq: 'monthly',
+			lastmod,
+			page: `${hostname}/recipe/${recipeId}`,
+			priority: '1.0'
+		});
+	});
+
+	Object.values(structuresCache).forEach((item) => {
+		body += getItemTemplate(item);
+	});
+	Object.values(tagsCache).forEach((item) => {
+		body += getItemTemplate(item);
+	});
+
+	body += '</urlset>';
+
+	return new Response(minifyInternal(body), {
+		headers: { 'Content-Type': 'application/xml' }
+	});
+}
+
+function getItemTemplate({
+	changefreq,
+	lastmod,
+	page,
+	priority = '0.5'
+}: SitemapItem) {
+	return `
+		<url>
+			<loc>https://${page}</loc>
+			<priority>${priority}</priority>
+			${changefreq ? `<changefreq>${changefreq}</changefreq>` : ''}
+			${lastmod ? `<lastmod>${lastmod}</lastmod>` : ''}
+		</url>
+	`;
 }
 
 export { GET };
