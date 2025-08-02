2b8c10c6f93a4ba9f2944cbd06c050444b9af8fe
Andrey Efimov <efiand@ya.ru>
2025-08-02 20:59:07 +0300

    Версионирование стилей [deploy]

--- server/lib/layout.js
+++ server/lib/layout.js
@@ -43,7 +43,7 @@ async function renderAssets(isAmp, hasForms) {
 	}
 
 	const linkTemplates = ALL_STYLESHEETS.map(
-		({ media, name }) => html`<link rel="stylesheet" href="/css/${name}.css" ${media ? `media="${media}"` : ""}>`,
+		({ media, name }) => html`<link rel="stylesheet" href="/css/${name}.css?1" ${media ? `media="${media}"` : ""}>`,
 	);
 	return html`
 		${linkTemplates.join("")}
