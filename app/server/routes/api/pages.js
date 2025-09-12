909b5225da9de3e64d1214cc2465932db7efbf04
Andrey Efimov <efiand@ya.ru>
2025-09-12 21:18:59 +0300

    Страница 404 для статики [deploy]

--- app/server/routes/api/pages.js
+++ app/server/routes/api/pages.js
@@ -2,11 +2,20 @@ import { sql } from "#common/utils/mark-template.js";
 import { processDb } from "#server/lib/db.js";
 
 const query = sql`
-	SELECT CONCAT('/structure/', id) AS url FROM structures
+	SELECT CONCAT('/structure/', id) AS url FROM structures s
+		WHERE (
+			SELECT count(id) FROM recipes WHERE structureId = s.id AND published = 1
+		) > 0
 	UNION
-	SELECT CONCAT('/tag/', id) AS url FROM tags
+	SELECT CONCAT('/tag/', id) AS url FROM tags t
+		WHERE (
+			SELECT count(id) FROM recipes WHERE id IN (
+				SELECT recipeId FROM recipesTags WHERE tagId = t.id
+			)
+			AND published = 1
+		) > 0
 	UNION
-	SELECT CONCAT('/recipe/', id) AS url FROM recipes
+	SELECT CONCAT('/recipe/', id) AS url FROM recipes WHERE published = 1
 	UNION
 	SELECT pathname AS url FROM staticPages
 	ORDER BY LENGTH(url), url;
