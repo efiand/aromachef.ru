2930c4e791b1a25881a30627a2e8f954abaa644a
Andrey Efimov <efiand@ya.ru>
2026-02-19 21:50:45 +0300

    Регистронезависимая подсветка [deploy]

--- app/common/lib/text.js
+++ app/common/lib/text.js
@@ -1,3 +1,12 @@
 export const capitalize = (text = "") => {
 	return `${text.slice(0, 1).toLocaleUpperCase()}${text.slice(1)}`;
 };
+
+export const highlight = (text = "", query = "") => {
+	if (!query) {
+		return text;
+	}
+
+	const regexp = new RegExp(`(${query.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})`, "gi");
+	return text.replace(regexp, /* html */ `<mark>$1</mark>`);
+};
--- app/server/routes/search.js
+++ app/server/routes/search.js
@@ -1,3 +1,4 @@
+import { highlight } from "#common/lib/text.js";
 import { renderCards } from "#common/templates/cards.js";
 import { renderPageSection } from "#common/templates/page-section.js";
 import { renderSearchForm } from "#common/templates/search-form.js";
@@ -5,10 +6,7 @@ import { isDev } from "#server/constants.js";
 import { processDb } from "#server/lib/db.js";
 
 const recipesQuery = /* sql */ `
-	SELECT
-		id,
-		REPLACE(title, ?, CONCAT('<mark>', ?, '</mark>')) as title
-	FROM recipes WHERE
+	SELECT id, title FROM recipes WHERE
 		(LOWER(title) LIKE CONCAT('%', LOWER(?), '%')
 		OR LOWER(description) LIKE CONCAT('%', LOWER(?), '%')
 		OR LOWER(ingredients) LIKE CONCAT('%', LOWER(?), '%')
@@ -34,7 +32,7 @@ export const searchRoute = {
 
 		if (body.fragment !== undefined) {
 			const cardsTemplate = nof
-				? renderCards({ cards: cards.slice(0, 4), isAmp })
+				? renderHighlightedCards(cards.slice(0, 4), pattern, isAmp)
 				: /* html */ `<p>Ничего не найдено.</p>`;
 			const buttonTemplate =
 				nof && nof > 4
@@ -50,10 +48,22 @@ export const searchRoute = {
 				description: "Ищите рецепты по заголовкам и содержимому.",
 				heading: "Поиск рецептов",
 				pageTemplate: renderPageSection({
-					footerTemplate: renderSearchForm({ nof, value: pattern }) + (nof ? renderCards({ cards, isAmp }) : ""),
+					footerTemplate:
+						renderSearchForm({ nof, value: pattern }) + (nof ? renderHighlightedCards(cards, pattern, isAmp) : ""),
 					title: "Поиск рецептов",
 				}),
 			},
 		};
 	},
 };
+
+/** @type {(cards: DbItem[], pattern: string, isAmp: boolean) => string} */
+function renderHighlightedCards(cards, pattern, isAmp) {
+	return renderCards({
+		cards: cards.map((card) => ({
+			...card,
+			title: highlight(card.title, pattern),
+		})),
+		isAmp,
+	});
+}
