5ab96bf9660e77d5c70c78f8e114cbbc4c6805bf
Andrey Efimov <efiand@ya.ru>
2026-03-27 21:17:16 +0300

    Обновление пакетов [deploy]

--- src/types/vendors.d.ts
+++ src/types/vendors.d.ts
@@ -1,1 +1,8 @@
 declare module 'petite-vue';
+declare module 'tinymce/icons/default';
+declare module 'tinymce/models/dom';
+declare module 'tinymce/plugins/autoresize';
+declare module 'tinymce/plugins/code';
+declare module 'tinymce/plugins/link';
+declare module 'tinymce/plugins/lists';
+declare module 'tinymce/themes/silver';
